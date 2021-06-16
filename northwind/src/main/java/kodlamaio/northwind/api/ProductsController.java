00411cd6d228adaec041d2987b9bf259bc741f4e
emre candan <wastorist@gmail.com>
2021-06-16 16:39:29 +0300

    last changes

--- northwind/src/main/java/kodlamaio/northwind/api/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/ProductsController.java
@@ -3,6 +3,7 @@ package kodlamaio.northwind.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import kodlamaio.northwind.entities.concretes.Product;
 import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/products")
 public class ProductsController {
 	
